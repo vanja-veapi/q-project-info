c4b530cfd53f1327539ff324c1ca393eb29c4d11
vanja-veapi <hrastivoje@gmail.com>
2022-03-21 10:24:58 +0100

    installed bootstrap and roboto font

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-	return (
-		<div className="App">
-			<p>Hello world</p>
-		</div>
-	);
+	return <div className="App"></div>;
 }
 
 export default App;
