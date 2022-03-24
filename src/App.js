028ca769233d8b597ef0b5947efcad401eb2f74e
vanja-veapi <hrastivoje@gmail.com>
2022-03-24 10:42:33 +0100

    finished register system and added JWT DECODE

--- src/App.js
+++ src/App.js
@@ -9,20 +9,29 @@ import { Routes, Route } from "react-router-dom";
 // Pages
 import MyProfile from "./components/pages/MyProfile/MyProfile";
 import Register from "./components/pages/Register/Register";
+// React Query
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+const queryClient = new QueryClient();
 
 function App() {
 	return (
-		<div className="App">
-			<div className="container-fluid">
-				<div className="w-100 h-auto bg-header">
-					<Header />
+		<QueryClientProvider client={queryClient}>
+			<div className="App">
+				<div className="container-fluid">
+					<div className="w-100 h-auto bg-header">
+						<Header />
+					</div>
+					<Routes>
+						<Route exact path="/" element={<MyProfile />} />
+						<Route path="/register" element={<Register />} />
+						{/* <Route path="/quantox" element={<QuantoxSpinner />} /> */}
+					</Routes>
 				</div>
-				<Routes>
-					<Route exact path="/" element={<MyProfile />} />
-					<Route path="/register" element={<Register />} />
-				</Routes>
 			</div>
-		</div>
+
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	);
 }
 
--- src/components/pages/Register/Register.js
+++ src/components/pages/Register/Register.js
@@ -2,11 +2,18 @@ import React, { useState } from "react";
 import "./Register.css";
 
 import FormRectangle from "../../elements/FormRectangle/FormRectangle";
+import QuantoxSpinner from "../../elements/QuantoxSpinner/QuantoxSpinner";
+
+// Custom query hooks
+import { useQuery } from "react-query";
+import { useRegisterUser } from "../../../hooks/users/RegisterUser/useRegisterUser";
 
 const Register = () => {
-	// Form values
+	// RegEx
 	const regExEmail = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/;
 	const regExGlobal = /^[A-ZŽŠĐČĆa-zžšđčć\d]{2,30}$/;
+
+	// UseStates
 	const [submitRegister, setSubmitRegister] = useState(false);
 	const [state, setState] = useState({
 		username: "",
@@ -16,52 +23,73 @@ const Register = () => {
 	});
 
 	const onRegister = () => {
-		console.log(state);
-		setSubmitRegister(true);
+		// If all regEx are true, then call react-query and register user
+		if (regExGlobal.test(state.username) && regExEmail.test(state.email) && regExGlobal.test(state.password) && state.password === state.confirmPassword) {
+			const newUser = {
+				username: state.username,
+				email: state.email,
+				password: state.password,
+			};
+			registerUser(newUser);
+			refetch();
+			return setSubmitRegister(false);
+		}
+
+		return setSubmitRegister(true);
 	};
 
+	const { mutate: registerUser } = useRegisterUser();
+
+	// const { data, isLoading, refetch } = useQuery("register-info", { enabled: false });
+	const { data, isLoading, refetch } = useQuery("register-info", { enabled: false, refetchOnMount: false, refetchOnWindowFocus: false });
+	console.log(data);
 	return (
-		<div className="register container-fluid position-relative">
-			<FormRectangle />
-			<div className="card">
-				<p className="h3 text-center mt-4">Register</p>
-				<div className="card-body text-start p-4">
-					{/* Username */}
-					<label className="mt-2">Username</label>
-					<br />
-					{!regExGlobal.test(state.username) && submitRegister ? <small className="text-danger">Username is required</small> : ""}
-					<input type="text" className="form-control" placeholder="Username" onChange={(e) => setState({ ...state, username: e.target.value })} />
+		<>
+			{isLoading ? <QuantoxSpinner /> : ""}
+			{isLoading ? document.body.classList.add("overflow-hidden") : document.body.classList.remove("overflow-hidden")}
 
-					{/* Email */}
-					<label className="mt-3">Email</label>
-					<br />
-					{!regExEmail.test(state.email) && submitRegister ? <small className="text-danger">Email is not in valid format</small> : ""}
-					<input type="text" className="form-control" placeholder="Email" onChange={(e) => setState({ ...state, email: e.target.value })} />
+			<div className="register container-fluid position-relative">
+				<FormRectangle />
+				<div className="card">
+					<p className="h3 text-center mt-4">Register</p>
+					<div className="card-body text-start p-4">
+						{/* Username */}
+						<label className="mt-2">Username</label>
+						<br />
+						{!regExGlobal.test(state.username) && submitRegister ? <small className="text-danger">Username is required</small> : ""}
+						<input type="text" className="form-control" placeholder="Username" onChange={(e) => setState({ ...state, username: e.target.value })} />
 
-					{/* Password */}
-					<label className="mt-3">Password</label>
-					<br />
-					{!regExGlobal.test(state.password) && submitRegister ? <small className="text-danger">Password is required</small> : ""}
-					<input type="text" className="form-control" placeholder="Password" onChange={(e) => setState({ ...state, password: e.target.value })} />
+						{/* Email */}
+						<label className="mt-3">Email</label>
+						<br />
+						{!regExEmail.test(state.email) && submitRegister ? <small className="text-danger">Email is not in valid format</small> : ""}
+						<input type="text" className="form-control" placeholder="Email" onChange={(e) => setState({ ...state, email: e.target.value })} />
 
-					{/* Confirm password */}
-					<label className="mt-3">Confirm Password</label>
-					<br />
+						{/* Password */}
+						<label className="mt-3">Password</label>
+						<br />
+						{!regExGlobal.test(state.password) && submitRegister ? <small className="text-danger">Password is required</small> : ""}
+						<input type="text" className="form-control" placeholder="Password" onChange={(e) => setState({ ...state, password: e.target.value })} />
 
-					{state.password !== state.confirmPassword && submitRegister ? <small className="text-danger">Confirm password valid must be same as password field</small> : ""}
-					<input type="text" className="form-control" placeholder="Confirm password" onChange={(e) => setState({ ...state, confirmPassword: e.target.value })} />
+						{/* Confirm password */}
+						<label className="mt-3">Confirm Password</label>
+						<br />
 
-					<div className="d-flex justify-content-end">
-						<button className="btn btn-success mt-3" onClick={onRegister}>
-							Register
-						</button>
+						{state.password !== state.confirmPassword && submitRegister ? <small className="text-danger">Confirm password valid must be same as password field</small> : ""}
+						<input type="text" className="form-control" placeholder="Confirm password" onChange={(e) => setState({ ...state, confirmPassword: e.target.value })} />
+
+						<div className="d-flex justify-content-end">
+							<button className="btn btn-success mt-3" onClick={onRegister}>
+								Register
+							</button>
+						</div>
+
+						{data?.error?.status >= 400 ? <div className="alert alert-danger mt-3">{data?.error?.message}</div> : ""}
+						{data?.success?.status < 400 ? <div className="alert alert-success mt-3">You have successfully registered</div> : ""}
 					</div>
-
-					{/* <div className="alert alert-success mt-3">Success message</div>
-					<div className="alert alert-danger mt-3">Error message</div> */}
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
