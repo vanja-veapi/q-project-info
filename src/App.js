6a1c37c203fc01180f0ca729c320976cda68b1eb
vanja-stojanovic-quantox <vanja.stojanovic@quantoxtechnology.com>
2022-03-25 16:59:47 +0100

    Finished Employee Project view and PM Project view

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 // Layouts
 import Header from "./components/layouts/Header/Header";
@@ -7,6 +8,8 @@ import Header from "./components/layouts/Header/Header";
 // Routes
 import { Routes, Route } from "react-router-dom";
 import MyProfile from "./components/pages/MyProfile/MyProfile";
+import ProjectView from "./components/pages/ProjectView/ProjectView";
+import CreateNote from "./components/pages/CreateNote/CreateNote";
 
 function App() {
 	return (
@@ -17,6 +20,7 @@ function App() {
 				</div>
 				<Routes>
 					<Route exact path="/" element={<MyProfile />} />
+					<Route exact path="/project" element={<ProjectView />} />
 				</Routes>
 			</div>
 		</div>
