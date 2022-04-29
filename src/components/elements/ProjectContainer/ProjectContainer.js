aea998011abbbe21c41af07390ea1d5b23e246e9
vanja-veapi <hrastivoje@gmail.com>
2022-04-29 09:41:34 +0200

    Dodao sam putanju do projectView-a

--- src/components/elements/ProjectContainer/ProjectContainer.js
+++ src/components/elements/ProjectContainer/ProjectContainer.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 import "./ProjectContainer.css";
 // IMG
 import rocket from "../../../assets/rocket.png";
@@ -65,12 +66,26 @@ const ProjectContainer = ({ bgColor, isPM }) => {
 							</div>
 
 							{/* If user has project role, then he'll see this button */}
-							{!isPM ? "" : <a className="btn btn-success" href="/create-project">New Project</a>}
+							{!isPM ? null : (
+								<a className="btn btn-success" href="/create-project">
+									New Project
+								</a>
+							)}
 						</div>
 					</div>
 				</div>
 			</div>
-			<div className="container container-project cards d-flex flex-wrap justify-content-center justify-content-lg-start">{projects?.length === 0 ? <h1>No project</h1> : projects?.map((project) => <ProjectCard key={project.id} name={project.attributes.name} logo={project.attributes.logo} countEmployees={project.attributes.employees?.data.length} user={fetchUser.data.data} />)}</div>
+			<div className="container container-project cards d-flex flex-wrap justify-content-center justify-content-lg-start">
+				{projects?.length === 0 ? (
+					<h1>No project</h1>
+				) : (
+					projects?.map((project) => (
+						<NavLink to={`/projects/${project.id}`}>
+							<ProjectCard key={project.id} name={project.attributes.name} logo={project.attributes.logo} countEmployees={project.attributes.employees?.data.length} user={fetchUser.data.data} />
+						</NavLink>
+					))
+				)}
+			</div>
 			{/* <div className="container container-project cards d-flex flex-wrap justify-content-center justify-content-lg-start">{html}</div> */}
 		</>
 	);
